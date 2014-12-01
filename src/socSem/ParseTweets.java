781c007f8ad1634e5f4abda0889772de4777cc29
cemaivaz <cemrifkiaydin@gmail.com>
2014-12-01 02:08:38 +0200

    for loops added

--- src/socSem/ParseTweets.java
+++ src/socSem/ParseTweets.java
@@ -7,8 +7,10 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class ParseTweets {
@@ -28,10 +30,13 @@ public class ParseTweets {
 		Set<String> twWords = new
 				HashSet<String>();
 
+		Map<String, ArrayList<String>> allHashes = new
+				HashMap<String, ArrayList<String>>();
+
 		try {
-//			BufferedReader br = new BufferedReader(new FileReader(new File("C:\\Users\\asus\\Documents\\training_set_tweets.txt")));
+			//			BufferedReader br = new BufferedReader(new FileReader(new File("C:\\Users\\asus\\Documents\\training_set_tweets.txt")));
 			BufferedReader br = new BufferedReader(new FileReader(new File("lim_tra_set.txt")));
-			
+
 			String line = "";
 			int cnt = 0;
 			while ((line = br.readLine()) != null && cnt++ < 1800) {
@@ -46,18 +51,44 @@ public class ParseTweets {
 
 				ArrayList<String> tmp =
 						new ArrayList<String>();
+
+				ArrayList<String> nonHashes = new
+						ArrayList<String>();
+
+				ArrayList<String> hashes = new
+						ArrayList<String>();
+
+
 				for (int i = 0; i < strArr.length; i++) {
 					if (strArr[i].length() > 1) {
-						if (wn.nouns.contains(strArr[i]) || strArr[i].charAt(0) == '#') {
+						if (wn.nouns.contains(strArr[i])) {
 							tmp.add(strArr[i]);
 							twWords.add(strArr[i]);
-						}
 
+							if (!nonHashes.contains(strArr[i]))
+								nonHashes.add(strArr[i]);
+						}
+						else if (strArr[i].charAt(0) == '#') {
+							tmp.add(strArr[i]);
+							hashes.add(strArr[i]);
+						}
 					}
 
 				}
 				if (tmp.size() > 0)
 					tweets.add(tmp);
+
+				if (hashes.size() > 1 && nonHashes.size() > 1) {
+					for (int i = 0; i < hashes.size(); i++) {
+						for (int j = 0; j < nonHashes.size(); j++) {
+							ArrayList<String> tmpHsh = (allHashes.containsKey(hashes.get(i))) ? 
+									allHashes.get(hashes.get(i)): new ArrayList<String>();
+							tmpHsh.add(nonHashes.get(j));
+							allHashes.put(hashes.get(i), tmpHsh);
+						}
+
+					}
+				}
 
 				//				System.out.println(tmp.toString());
 				//				System.out.println(line);
@@ -67,6 +98,11 @@ public class ParseTweets {
 			System.out.println(twWords.size());
 			br.close();
 
+			
+			ArrayList<String> hashKeys = new ArrayList<String>(allHashes.keySet());
+			for (int i = 0; i < allHashes.size(); i++) {
+				
+			}
 
 
 			FileWriter fw = 
@@ -80,8 +116,8 @@ public class ParseTweets {
 			for (int i = 0; i < tweets.size(); i++) {
 				ArrayList<String> twAl = tweets.get(i);
 				String res = "";
-				
-				
+
+
 				int cntHasht = 0;
 				for (int j = 0; j < twAl.size(); j++) {
 					if (twAl.get(j).charAt(0) == '#')
@@ -91,33 +127,33 @@ public class ParseTweets {
 					else
 						fw.write(twAl.get(j) + ",");
 				}
-				
+
 				if (cntHasht == twAl.size())
 					System.out.println();
 				else if (cntHasht == 0)
 					res = wn.simpleHypSum(twAl);
 				else
 					System.out.println();
-				
-				
+
+
 				System.out.println(twAl.toString() + "//////" + res);
-				
-				
+
+
 				fw.write(" | hypernyms: ");
-				
-				
-				
+
+
+
 				fw.write("\n");
 			}
 
 
-//			System.out.println("|||||____");
-//			System.out.println(wn.hypernym("france"));
-//			System.out.println("***");
-//			System.out.println(wn.hypernymy.get("france"));
-			
-			
-			
+			//			System.out.println("|||||____");
+			//			System.out.println(wn.hypernym("france"));
+			//			System.out.println("***");
+			//			System.out.println(wn.hypernymy.get("france"));
+
+
+
 			fw.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
